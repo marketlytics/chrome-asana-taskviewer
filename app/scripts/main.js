07570d61e43a9d2756860cea15c9ccc981548986
Mashhood Rastgar <mashhoodr@gmail.com>
2014-11-23 14:58:39 +0500

    Set minWidth and minHeight for window

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,11 +8,14 @@ function createAppWindow() {
         'alwaysOnTop': true,
         'focused': false,
         frame: 'none',
-        bounds: {
+        singleton: true,
+        outerBounds: {
             width: 350,
             height: 350,
             left: Math.round((screen.availWidth - 350) / 2),
-            top: Math.round((screen.availHeight - 350)/2)
+            top: Math.round((screen.availHeight - 350)/2),
+            minWidth: 350,
+            minHeight: 110
         }
     });
 }
@@ -33,37 +36,37 @@ chrome.commands.onCommand.addListener(function(command) {
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
 
-  chrome.storage.onChanged.addListener(function(changes, areaName) {
-    if(areaName === 'local') {
-        if(typeof changes.apiKey !== 'undefined') { // apiKey has changed
-            var mainWindow = chrome.app.window.get('main');
-            if(mainWindow !== null) {
-                mainWindow.close(); // restart the app
+    chrome.storage.onChanged.addListener(function(changes, areaName) {
+        if(areaName === 'local') {
+            if(typeof changes.apiKey !== 'undefined') { // apiKey has changed
+                var mainWindow = chrome.app.window.get('main');
+                if(mainWindow !== null) {
+                    mainWindow.close(); // restart the app
+                }
+                setTimeout(function() {
+                    createAppWindow();
+                }, 1000);
+                
             }
-            setTimeout(function() {
-                createAppWindow();
-            }, 1000);
-            
         }
-    }
-  });
+    });
 
-  chrome.storage.local.get('apiKey', function(value) {
-    if(typeof value.apiKey !== 'undefined' && value.apiKey !== '') {
-        createAppWindow();
-    } else {
-        chrome.app.window.create('auth.html', {
-            id: 'auth',
-            'focused': true,
-            bounds: {
-              width: 500,
-              height: 140,
-              left: Math.round((screen.availWidth - 500) / 2),
-              top: Math.round((screen.availHeight - 140)/2)
-            }
-        });
+    chrome.storage.local.get('apiKey', function(value) {
+        if(typeof value.apiKey !== 'undefined' && value.apiKey !== '') {
+            createAppWindow();
+        } else {
+            chrome.app.window.create('auth.html', {
+                id: 'auth',
+                'focused': true,
+                bounds: {
+                  width: 500,
+                  height: 140,
+                  left: Math.round((screen.availWidth - 500) / 2),
+                  top: Math.round((screen.availHeight - 140)/2)
+                }
+            });
 
-    }
-  });
+        }
+    });
 
 });
