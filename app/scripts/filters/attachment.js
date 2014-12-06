13c561aeb58b1e73c69018c4c9cfdf70b27d7707
Mashhood Rastgar <mashhoodr@gmail.com>
2014-12-06 10:30:24 +0500

    support for clickable urls in comments

--- app/scripts/filters/attachment.js
+++ app/scripts/filters/attachment.js
@@ -3,13 +3,17 @@
 */
 angular.module('asanaChromeApp').
 filter('attachment', function() {
-  return function(storyText) {
-  	
-  	if(storyText.indexOf('attached') !== -1) {
-  		var url = storyText.match(/http.*/gm);
-  		if(url.length > 0)
-  			storyText = storyText.replace(url, '<a href="' + url[0] + '" target="_blank">this file</a>');
-  	}
+  return function(storyText) {  	
+		var url = storyText.match(/([-a-zA-Z0-9^\p{L}\p{C}\u00a1-\uffff@:%_\+.~#?&//=]{2,256}){1}(\.[a-z]{2,4}){1}(\:[0-9]*)?(\/[-a-zA-Z0-9\u00a1-\uffff\(\)@:%,_\+.~#?&//=]*)?([-a-zA-Z0-9\(\)@:%,_\+.~#?&//=]*)?/gm);
+		if(url && url.length > 0) {
+      if(storyText.indexOf('attached') !== -1) { 
+			 storyText = storyText.replace(url[0], '<a href="' + url[0] + '" target="_blank">this file</a>');
+      } else {
+        for(var x = 0; x < url.length; x++) {
+          storyText = storyText.replace(url[x], '<a href="' + url[x] + '" target="_blank">' + url[x] + '</a>');
+        }
+      }
+    }
     return storyText;
   };
 });
