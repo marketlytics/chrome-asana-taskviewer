ef872940356333f34f40647dfbc4707a50df5d7c
Mashhood Rastgar <mashhoodr@gmail.com>
2015-01-17 15:43:13 +0500

    introduced tracking of resolution changes

--- app/scripts/chrome.js
+++ app/scripts/chrome.js
@@ -1,8 +1,16 @@
 // var fs = null;
 // var FOLDERNAME = 'testchromeapp';
+// &uid ==> user id
 
+/* Analytics library in bower was manually updated to 1.5.2 since bower couldnt find it */
 var service = analytics.getService('asana-chrome-app');
-window.tracker = service.getTracker('UA-18735851-11'); 
+window.tracker = service.getTracker('UA-18735851-11');
+var resolutionLabel = window.innerWidth + ' x ' + window.innerHeight;
+var trackResolution = analytics.EventBuilder.builder()
+.category('app')
+.action('resolution')
+.dimension(1, resolutionLabel);
+tracker.send(trackResolution.label(resolutionLabel));
 
 function onError(e) {
   console.error('Error!', e);
